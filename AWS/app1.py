3c4a6d2a2dc3b31ae593730095673397361a5e13
Rishiande <96639486+Rishiande@users.noreply.github.com>
2025-06-30 15:14:26 +0530

    Update app1.py

--- AWS/app1.py
+++ AWS/app1.py
@@ -5,7 +5,7 @@ import requests
 # Load the CSV file
 @st.cache_data
 def load_data():
-    file_path = 'CMApprovalGoodBad-CrossTabs-Weighted-ByRows'
+    file_path = 'CMApprovalGoodBad-CrossTabs-Weighted-ByRows.csv'
     data = pd.read_csv(file_path)
     return data
 
